bef746d3198b503bf9e6029b7911eec0a1230c55
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-07-17 10:59:44 -0600

    Add Test Project

---
+++ AdoptiverseEndpointTests/Program.cs
@@ -0,0 +1,41 @@
+﻿using AdoptiverseAPI.DataAccess;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AdoptiverseAPI.Controllers;
+
+namespace AdoptiverseEndpointTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+                        Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureServices(services =>
+                    {
+                        //THIS IS THE CODE TO ADD:
+                        services.AddDbContext<AdoptiverseApiContext>(options =>
+                            options.UseInMemoryDatabase("TestDatabase"));
+
+                        services.AddControllers().AddApplicationPart(typeof(SheltersController).Assembly);
+                    });
+
+                    webBuilder.Configure(app =>
+                    {
+                        app.UseRouting();
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapControllers();
+                        });
+                    });
+                });
+    }
+}
---
+++ AdoptiverseEndpointTests/ShelterCrudEndpointTests.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace AdoptiverseEndpointTests
+{
+    public class ShelterCrudEndpointTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+
+    }
+}
